2c07c6a64554007d946290360674233e7c6ce46a
ggallant <ggallant@dovico.com>
2012-11-09 09:25:49 -0400

    Moved the text, that indicates to the user where to find their data access token, to the common library. Updated the version number of the app too.

--- SubmitEmployeeTime/src/com/dovico/submitemployeetime/CPanel_SettingsEx.java
+++ SubmitEmployeeTime/src/com/dovico/submitemployeetime/CPanel_SettingsEx.java
@@ -1,40 +1,16 @@
 package com.dovico.submitemployeetime;
 
-import java.awt.*;
-import javax.swing.*;
-
 import com.dovico.commonlibrary.CPanel_Settings;
 
 
 public class CPanel_SettingsEx extends CPanel_Settings {
 	private static final long serialVersionUID = 1L;
 	
-	private JLabel m_lblLine1 = null;
-	private JLabel m_lblLine2 = null;
-	private JLabel m_lblLine3 = null;
-	
+
 	// Default constructor
 	public CPanel_SettingsEx() {
 		// Call the parent class's constructor (handles setting up the controls) 
 		super();
-		
-		m_lblLine1 = new JLabel("This App requires administrator API tokens from DOVICO Timesheet\u2122 or DOVICO Planning and Timesheet\u2122");
-		m_lblLine1.setHorizontalAlignment(SwingConstants.CENTER);
-		m_lblLine1.setFont(new Font("Arial", Font.PLAIN, 11));
-		m_lblLine1.setBounds(10, 84, 698, 14);
-		add(m_lblLine1);
-		
-		m_lblLine2 = new JLabel("via Database Options (Menu > Setup > Database Options > API)");
-		m_lblLine2.setHorizontalAlignment(SwingConstants.CENTER);
-		m_lblLine2.setFont(new Font("Arial", Font.PLAIN, 11));
-		m_lblLine2.setBounds(10, 101, 698, 14);
-		add(m_lblLine2);
-		
-		m_lblLine3 = new JLabel("");
-		m_lblLine3.setHorizontalAlignment(SwingConstants.CENTER);
-		m_lblLine3.setFont(new Font("Arial", Font.PLAIN, 11));
-		m_lblLine3.setBounds(10, 118, 698, 14);
-		add(m_lblLine3);
 	}
 	
 	
@@ -48,20 +24,6 @@ public class CPanel_SettingsEx extends CPanel_Settings {
 		
 		// Pass the information to the parent class that it needs
 		super.setSettingsData(sConsumerSecretToUse, sDataAccessToken, sApiVersionTargeted, lEmployeeID, sEmployeeFirstName, sEmployeeLastName, bHideConsumerSecretField);
-		
-		
-		// If we hid the Consumer Secret field then...
-		if(bHideConsumerSecretField){
-			// We don't need to tell the user where to find the Admin tokens, we need to tell the user where to find the Data Access Token (we only need to adjust
-			// the text in lines 1 and 3 - line 2 is fine)
-			m_lblLine1.setText("This App requires a Data Access Token from DOVICO Timesheet\u2122 or DOVICO Planning and Timesheet\u2122");
-			m_lblLine3.setText("or via My Time & Expenses / Options (Menu > Views > My Time & Expenses > Options)");
-			
-			// Let's also tweak the vertical position of the controls to place them a bit higher on the page since the Consumer Secret field is not present
-			m_lblLine1.setBounds(10, (84 - 30), 698, 14);
-			m_lblLine2.setBounds(10, (101 - 30), 698, 14);
-			m_lblLine3.setBounds(10, (118 - 30), 698, 14);
-		} // End if(bHideConsumerSecretField)
 	}
 
 	
